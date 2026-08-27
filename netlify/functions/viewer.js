9d0630ce8e667a6ea991612404ce054a5d1f8556
RazaRathour <166321138+RazaRathour@users.noreply.github.com>
2026-08-27 12:59:56 +0500

    Create viewer.js

---
+++ netlify/functions/viewer.js
@@ -0,0 +1,3 @@
+git add netlify.toml package.json netlify/functions/viewer.js netlify/functions/package.json facebook-image-link-tool.html viewer.html
+git commit -m "Fix Netlify deployment - add functions and config"
+git push
